74d2dc3a842449093094fc4c665d80a35ec9f285
Iryna Stein <isophiestein@gmail.com>
2021-03-18 20:19:01 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,3 @@
-# Define the variable board below.
-row = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-row [0]
-row [1]
-row [2]
+def board
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  puts board
